a1e51d25f70ff77aa0a9484fb489e261b7d5784a
muhamedkarajic <muhamed.karajic@edu.fit.ba>
2022-07-31 20:15:09 +0200

    Initial commit

---
+++ WebAPI/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Serilog;
+global using Serilog.Events;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using System.Threading;
+global using SerilogTimings;
+global using System;
