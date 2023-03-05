4626abd1f7b70727efc04fb2d3fdc9eed7f76028
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2023-03-05 01:55:12 +0100

    Update Global.cs

--- WebAPI/Global.cs
+++ WebAPI/Global.cs
@@ -4,19 +4,32 @@ namespace Global;
 
 public class MyHub : Hub
 {
-    static int TotalUsers = 0;
-    public async Task NewWindowLoaded(string payload)
-    {
-        TotalUsers++;
+    static Dictionary<string, DateTime> users = new Dictionary<string, DateTime>();
 
-        await Clients.All.SendAsync("updateTotalViews", TotalUsers);
+    public async Task ConnectX()
+    {
+        var connectionId = base.Context.ConnectionId;
+
+        DateTime dateTime = DateTime.Now;
+        
+        if(users.TryGetValue(connectionId, out DateTime existingDateTime)) {
+            dateTime = existingDateTime;
+        }
+        else
+        {
+            users.Add(connectionId, dateTime);
+        }
+
+        await Clients.All.SendAsync("connectedUsers", users.Count());
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        TotalUsers--;
+        var connectionId = base.Context.ConnectionId;
+        
+        users.Remove(connectionId);
 
-        await Clients.All.SendAsync("updateTotalViews", TotalUsers);
+        await Clients.All.SendAsync("connectedUsers", users.Count());
 
         await base.OnDisconnectedAsync(exception);
     }
